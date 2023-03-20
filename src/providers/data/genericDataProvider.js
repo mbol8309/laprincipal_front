ed30263317bf9f7db41ac9f1843e2c34e0d0d1d8
Miguel Bolivar <mbol8309@gmail.com>
2023-03-20 22:47:23 +0100

    insert and update

--- src/providers/data/genericDataProvider.js
+++ src/providers/data/genericDataProvider.js
@@ -2,37 +2,68 @@ import dataProvider from ".";
 import instance from "../../api/instance";
 
 const genericDataProvider = {
-    //----------------LIST
-    getList: (resource, params) =>{
-        const {page, perPage} = params.pagination;
-        const {field, order} = params.sort;
+  //----------------LIST
+  getList: (resource, params) => {
+    const { page, perPage } = params.pagination;
+    const { field, order } = params.sort;
 
-        return instance.post('getAll',{
-            model:resource,
-            per_page: perPage,
-            page,
-            field, order
-        }).then(({data})=>{
-            return {
-                data: data?.data,
-                total:data?.total
-            }
-        })
-    },
-    //-----------------ONE
-    getOne:(resource,params) => {
-        const {id,...filters} = params;
-        return instance.post('getById',{
-            model:resource,
-            id,
-            ...filters
-        }).then(({data})=>{
-            return {
-                data:data.data
-            };
-        })
-    }
+    return instance
+      .post("getAll", {
+        model: resource,
+        per_page: perPage,
+        page,
+        field,
+        order,
+      })
+      .then(({ data }) => {
+        return {
+          data: data?.data,
+          total: data?.total,
+        };
+      });
+  },
+  //-----------------ONE
+  getOne: (resource, params) => {
+    const { id, ...filters } = params;
+    return instance
+      .post("getById", {
+        model: resource,
+        id,
+        ...filters,
+      })
+      .then(({ data }) => {
+        return {
+          data: data.data,
+        };
+      });
+  },
+  update: (resource, params) => {
+    const { id, data, previousData, meta } = params;
+    return instance
+      .post("updateById", {
+        model: resource,
+        id,
+        data,
+      })
+      .then(({ data }) => {
+        return {
+          data: data.data,
+        };
+      });
+  },
+  create: (resource, params) => {
+    const { data, meta } = params;
+    return instance
+      .post("insert", {
+        model: resource,
+        data,
+      })
+      .then(({ data }) => {
+        return {
+          data: data.data,
+        };
+      });
+  },
+};
 
-}
-
-export default genericDataProvider
+export default genericDataProvider;
